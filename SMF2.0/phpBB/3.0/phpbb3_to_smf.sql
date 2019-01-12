a55c154c1b2790602150dca82e18399101dd8623
vbgamer45 <vbgamer45@gmail.com>
2019-01-12 09:25:19 -0600

    !PHPBB 3.0.x handle null values better when inserting data into smf_members table.
    Signed-off-by: vbgamer45 <vbgamer45@gmail.com>

--- SMF2.0/phpBB/3.0/phpbb3_to_smf.sql
+++ SMF2.0/phpBB/3.0/phpbb3_to_smf.sql
@@ -265,7 +265,7 @@ SELECT
 	'' AS lngfile, '' AS buddy_list, '' AS pm_ignore_list, '' AS message_labels,
 	'' AS personal_text, '' AS time_format, '' AS usertitle, u.user_ip AS member_ip,
 	'' AS secret_question, '' AS secret_answer, '' AS validation_code,
-	'' AS additional_groups, '' AS smiley_set, '' AS password_salt,
+	'' AS additional_groups, '' AS smiley_set, '' AS password_salt, '' as openid_uri, '' AS ignore_boards, 
 	u.user_ip AS member_ip2
 FROM {$from_prefix}users AS u
 	LEFT JOIN {$from_prefix}ranks AS r ON (r.rank_id = u.user_rank AND r.rank_special = 1)
