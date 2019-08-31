2e55006f69bfad01545a50e2464ba0b21b3545ca
vbgamer45 <vbgamer45@gmail.com>
2019-08-31 10:01:12 -0500

    Added IPB 4.4 converter beta.
    Signed-off-by: vbgamer45 <vbgamer45@gmail.com>

--- SMF2.0/Invision/4.4/ipb44_to_smf.sql
+++ SMF2.0/Invision/4.4/ipb44_to_smf.sql
@@ -1,13 +1,13 @@
 /* ATTENTION: You don't need to run or use this file!  The convert.php script does everything for you! */
 
 /******************************************************************************/
----~ name: "Invision Power Board 3.2"
+---~ name: "Invision Power Board 4.4"
 /******************************************************************************/
 ---~ version: "SMF 2.0"
 ---~ settings: "/conf_global.php"
 ---~ globals: INFO
 ---~ from_prefix: "`$INFO[sql_database]`.$INFO[sql_tbl_prefix]"
----~ table_test: "{$from_prefix}members"
+---~ table_test: "{$from_prefix}core_members"
 
 /******************************************************************************/
 --- Converting members...
@@ -17,16 +17,18 @@ TRUNCATE {$to_prefix}members;
 TRUNCATE {$to_prefix}attachments;
 
 ---* {$to_prefix}members
+---{
+
+$row['last_login'] = (int) $row['last_login'];
+---}
 SELECT
 	m.member_id AS id_member, SUBSTRING(m.name, 1, 80) AS member_name,
-	SUBSTRING(m.members_display_name, 1, 255) AS real_name, m.email AS email_address,
+	SUBSTRING(m.name, 1, 255) AS real_name, m.email AS email_address,
 	SUBSTRING(m.members_pass_hash, 1, 64) AS passwd, SUBSTRING(m.members_pass_salt, 1, 8) AS password_salt, 
-        m.title AS usertitle, m.last_visit AS last_login, 
+        m.member_title AS usertitle, m.last_visit AS last_login, 
         m.joined AS date_registered, SUBSTRING(pc.field_3, 1, 255) AS website_url,
 	SUBSTRING(pc.field_3, 1, 255) AS website_title,
-	SUBSTRING(pc.field_4, 1, 255) AS icq, SUBSTRING(pc.field_1, 1, 16) AS aim,
-	SUBSTRING(pc.field_8, 1, 32) AS yim, SUBSTRING(pc.field_2, 1, 255) AS msn,
-	SUBSTRING(pp.signature, 1, 65534) AS signature, m.ip_address AS member_ip, m.ip_address AS member_ip2, '0' AS total_time_logged_in,
+	SUBSTRING(m.signature, 1, 65534) AS signature, m.ip_address AS member_ip, m.ip_address AS member_ip2, '0' AS total_time_logged_in,
 	IF (m.bday_year IS NULL, '0001-01-01', IF (m.bday_year = 0 AND m.bday_month != 0 AND m.bday_day != 0, CONCAT('0004-', m.bday_month, '-', m.bday_day), CONCAT_WS('-', IF(m.bday_year <= 4, '0001', m.bday_year), IF(m.bday_month = 0, '01', IF(m.bday_month < 10, CONCAT('0', m.bday_month), m.bday_month)), IF(m.bday_day = 0, '01', IF(m.bday_day < 10, CONCAT('0', m.bday_day), m.bday_day))))) AS birthdate,
         CASE
 		WHEN (m.member_group_id = '4') THEN 1
@@ -39,9 +41,9 @@ SELECT
                 WHEN (pc.field_5 = 'f') THEN 2
 		ELSE 0
 	END AS gender
-FROM {$from_prefix}members AS m
-     LEFT JOIN {$from_prefix}pfields_content AS pc ON (pc.member_id = m.member_id)
-     LEFT JOIN {$from_prefix}profile_portal AS pp ON (pp.pp_member_id = m.member_id);
+FROM {$from_prefix}core_members AS m
+     LEFT JOIN {$from_prefix}core_pfields_content AS pc ON (pc.member_id = m.member_id)
+;
 
 ---{
 // Get the buddies.
@@ -102,8 +104,22 @@ if (!empty($row['additional_groups']))
 TRUNCATE {$to_prefix}categories;
 
 ---* {$to_prefix}categories
-SELECT id AS id_cat, SUBSTRING(name, 1, 255) AS name, position AS cat_order
-FROM {$from_prefix}forums
+---{
+
+$result2 = convert_query("select * from {$from_prefix}core_sys_lang_words where word_key ='forums_forum_" . $row['id_cat'] . "'");
+while ($row2 = convert_fetch_assoc($result2))
+{
+	if (!empty($row2['word_default']))
+	{
+		$row['name'] = $row2['word_default'];
+	}
+}
+
+
+---}
+
+SELECT id AS id_cat, SUBSTRING(name_seo, 1, 255) AS name, position AS cat_order
+FROM {$from_prefix}forums_forums
 WHERE parent_id = -1;
 ---*
 
@@ -118,13 +134,34 @@ WHERE id_group > 4;
 
 /* The converter will set id_cat for us based on id_parent being wrong. */
 ---* {$to_prefix}boards
+---{
+// IPB stores forum names and descriptions in a seperate database table.
+$result2 = convert_query("select * from {$from_prefix}core_sys_lang_words where word_key ='forums_forum_" . $row['id_board'] . "' OR word_key ='forums_forum_" . $row['id_board'] . "_desc'");
+while ($row2 = convert_fetch_assoc($result2))
+{
+	if (!empty($row2['word_default'])  && $row2['word_key'] == ('forums_forum_' . $row['id_board']))
+	{
+		$row['name'] = $row2['word_default'];
+	}
+	
+	
+	if (!empty($row2['word_default'])  && $row2['word_key'] == ('forums_forum_' . $row['id_board'] . '_desc') )
+	{
+		$row['description'] = $row2['word_default'];
+	}
+	
+}
+
+
+---}
+
 SELECT
-	b.id AS id_board, SUBSTRING(b.name, 1, 255) AS name,
-	SUBSTRING(b.description, 1, 65534) AS description, b.position AS board_order,
+	b.id AS id_board, SUBSTRING(b.name_seo, 1, 255) AS name,
+	SUBSTRING(b.name_seo, 1, 65534) AS description, b.position AS board_order,
 	b.posts AS num_posts, IF (p.parent_id = -1, 0, p.parent_id) AS id_parent, b.inc_postcount AS count_posts, 
         '-1,0' AS member_groups
-FROM {$from_prefix}forums AS b
-        LEFT JOIN {$from_prefix}forums AS p ON (b.parent_id = p.id)
+FROM {$from_prefix}forums_forums AS b
+        LEFT JOIN {$from_prefix}forums_forums AS p ON (b.parent_id = p.id)
 WHERE b.parent_id != -1;
 ---*
 
@@ -146,8 +183,8 @@ SELECT
 	IFNULL(pl.pid,0) AS id_poll, t.posts AS num_replies, t.views AS num_views,
 	MIN(p.pid) AS id_first_msg, MAX(p.pid) AS id_last_msg,
 	t.state = 'closed' AS locked, approved
-FROM ({$from_prefix}topics AS t, {$from_prefix}posts AS p)
-	LEFT JOIN {$from_prefix}polls AS pl ON (pl.tid = t.tid)
+FROM ({$from_prefix}forums_topics AS t, {$from_prefix}forums_posts AS p)
+	LEFT JOIN {$from_prefix}core_polls AS pl ON (pl.pid = t.poll_state)
 WHERE p.topic_id = t.tid
 GROUP BY t.tid
 HAVING id_first_msg != 0
@@ -164,10 +201,11 @@ TRUNCATE {$to_prefix}attachments;
 ---* {$to_prefix}messages
 ---{
 $ignore = true;
-/* Needs updating
+/*
 $row['body'] = preg_replace('~\[quote name=(.+?) timestamp=(.+?) post=(.+?)\]~ie', '\'[quote author=\' . substr(\'$1\', 5, -5) . \' link=topic=\'. $row[\'id_topic\'].\'.msg\' . substr(\'$3\', 5, -5) . \'#msg\' . substr(\'$3\', 5, -5) . \' date=\' . substr(\'$2\', 5, -5) . \']\'', $row['body']);
 $row['body'] = preg_replace("~<img .*?bbc_emoticon' alt='(.+?)' />~ie", '\'$1\'', $row['body']);
 $row['body'] = preg_replace("~<img .*?emoid=\"(.+?)\".*?/>~ie", '\'$1\'', $row['body']);
+
 */
 ---}
 SELECT
@@ -178,9 +216,9 @@ SELECT
 	IFNULL(p.edit_time, 0) AS modified_time, SUBSTRING(p.edit_name, 1, 255) AS modified_name,
 	t.forum_id AS id_board, REPLACE(p.post, '<br />', '') AS body,
 	SUBSTRING(IFNULL(m.email, 'guest@example.com'), 1, 255) AS poster_email, 'xx' AS icon
-FROM {$from_prefix}posts AS p
-	LEFT JOIN {$from_prefix}topics AS t ON (t.tid = p.topic_id)
-	LEFT JOIN {$from_prefix}members AS m ON (m.member_id = p.author_id)
+FROM {$from_prefix}forums_posts AS p
+	LEFT JOIN {$from_prefix}forums_topics AS t ON (t.tid = p.topic_id)
+	LEFT JOIN {$from_prefix}core_members AS m ON (m.member_id = p.author_id)
 WHERE t.forum_id IS NOT NULL;
 ---*
 
@@ -214,8 +252,8 @@ TRUNCATE {$to_prefix}log_polls;
 SELECT
 	p.pid AS id_poll, SUBSTRING(p.poll_question, 1, 255) AS question,
 	p.starter_id AS id_member, SUBSTRING(IFNULL(m.name, 'Guest'), 1, 255) AS poster_name
-FROM {$from_prefix}polls AS p
-	LEFT JOIN {$from_prefix}members AS m ON (m.member_id = p.starter_id);
+FROM {$from_prefix}core_polls AS p
+	LEFT JOIN {$from_prefix}core_members AS m ON (m.member_id = p.starter_id);
 ---*
 
 /******************************************************************************/
@@ -267,7 +305,7 @@ if (is_array($choices))
 }
 ---}
 SELECT pid AS id_poll, choices
-FROM {$from_prefix}polls;
+FROM {$from_prefix}core_polls;
 ---*
 
 /******************************************************************************/
@@ -278,10 +316,10 @@ FROM {$from_prefix}polls;
 ---{
 $ignore = true;
 ---}
-SELECT pl.pid AS id_poll, v.member_id AS id_member
-FROM {$from_prefix}voters AS v
-	LEFT JOIN {$from_prefix}polls AS pl ON (pl.tid = v.tid)
-WHERE v.member_id != 0;
+SELECT v.poll AS id_poll, v.member_id AS id_member
+FROM {$from_prefix}core_voters AS v
+	
+WHERE v.member_id != 0 ;
 ---*
 
 /******************************************************************************/
@@ -293,7 +331,7 @@ TRUNCATE {$to_prefix}personal_messages;
 ---* {$to_prefix}personal_messages
 ---{
 $ignore = true;
-/* Needs updating
+/*
 $row['body'] = preg_replace('~\[quote name=(.+?) timestamp=(.+?) post=(.+?)\]~ie', '\'[quote author=\' . substr(\'$1\', 5, -5) . \' link=topic=\'. $row[\'id_topic\'].\'.msg\' . substr(\'$3\', 5, -5) . \'#msg\' . substr(\'$3\', 5, -5) . \' date=\' . substr(\'$2\', 5, -5) . \']\'', $row['body']);
 $row['body'] = preg_replace("~<img .*?bbc_emoticon' alt='(.+?)' />~ie", '\'$1\'', $row['body']);
 $row['body'] = preg_replace("~<img .*?emoid=\"(.+?)\".*?/>~ie", '\'$1\'', $row['body']);
@@ -305,9 +343,9 @@ SELECT
         SUBSTRING(IFNULL(m.name, "Guest"), 1, 255) AS from_name,
 	SUBSTRING(mt.mt_title, 1, 255) AS subject,
 	SUBSTRING(pm.msg_post, 1, 65534) AS body
-FROM {$from_prefix}message_topics AS mt
-        LEFT JOIN {$from_prefix}message_posts AS pm ON (mt.mt_id = pm.msg_topic_id)
-        LEFT JOIN {$from_prefix}members AS m ON (m.member_id = pm.msg_author_id)
+FROM {$from_prefix}core_message_topics AS mt
+        LEFT JOIN {$from_prefix}core_message_posts AS pm ON (mt.mt_id = pm.msg_topic_id)
+        LEFT JOIN {$from_prefix}core_members AS m ON (m.member_id = pm.msg_author_id)
 WHERE mt.mt_is_draft = 0
 	AND pm.msg_id > 0
 	AND pm.msg_id IS NOT NULL;
@@ -348,117 +386,16 @@ if (is_array($invited_members) && !empty($invited_members))
 ---}
 SELECT pm.msg_id AS id_pm, mt.mt_to_member_id AS id_member, '-1' AS labels, 
         IF(IFNULL(mtum.map_has_unread, 1) > 0, 0, 1) AS is_read,
-        mt.mt_invited_members AS invited_members, pm.msg_author_id, mt.mt_starter_id, IF(mt.mt_is_deleted = 1 AND mt.mt_starter_id = pm.msg_author_id, 1, 0) AS deleted
-FROM {$from_prefix}message_topics AS mt
-    LEFT JOIN {$from_prefix}message_posts AS pm ON (mt.mt_id = pm.msg_topic_id)
-    LEFT JOIN {$from_prefix}message_topic_user_map AS mtum ON (mtum.map_topic_id = mt.mt_id AND mtum.map_user_id = mt.mt_to_member_id)
+        '0' AS invited_members, pm.msg_author_id, mt.mt_starter_id, IF(mt.mt_is_deleted = 1 AND mt.mt_starter_id = pm.msg_author_id, 1, 0) AS deleted
+FROM {$from_prefix}core_message_topics AS mt
+    LEFT JOIN {$from_prefix}core_message_posts AS pm ON (mt.mt_id = pm.msg_topic_id)
+    LEFT JOIN {$from_prefix}core_message_topic_user_map AS mtum ON (mtum.map_topic_id = mt.mt_id AND mtum.map_user_id = mt.mt_to_member_id)
 WHERE mt.mt_is_draft = 0
 	AND pm.msg_id > 0
 	AND pm.msg_id IS NOT NULL;
 ---*
 
-/******************************************************************************/
---- Converting censored words...
-/******************************************************************************/
 
-DELETE FROM {$to_prefix}settings
-WHERE variable IN ('censor_vulgar', 'censor_proper');
-
----# Moving censored words...
----{
-$result = convert_query("
-	SELECT type, swop
-	FROM {$from_prefix}badwords");
-$censor_vulgar = array();
-$censor_proper = array();
-
-while ($row = convert_fetch_assoc($result))
-{
-	$type = '';
-	$swop = '';
-	if (mb_detect_encoding($row['type']) == 'UTF-8' && isset($db_character_set))
-	{
-		$type = trim($row['type']);
-		$swop = trim($row['swop']);
-	}
-	elseif (mb_detect_encoding($row['type']) == 'UTF-8' && !isset($db_character_set))
-	{
-		$type = trim(utf8_decode($row['type']));
-		$swop = trim(utf8_decode($row['swop']));
-	}
-	elseif (mb_detect_encoding($row['type']) != 'UTF-8' && isset($db_character_set))
-	{
-		$type = trim(utf8_encode($row['type']));
-		$swop = trim(utf8_encode($row['swop']));
-	}
-	else
-	{
-		$type = trim($row['type']);
-		$swop = trim($row['swop']);
-	}
-
-	if (!empty($type))
-	{
-		$censor_vulgar[] = $type;
-		$censor_proper[] = $swop;
-	}
-}
-convert_free_result($result);
-
-$censored_vulgar = addslashes(implode("\n", $censor_vulgar));
-$censored_proper = addslashes(implode("\n", $censor_proper));
-
-convert_query("
-	REPLACE INTO {$to_prefix}settings
-		(variable, value)
-	VALUES ('censor_vulgar', '$censored_vulgar'),
-		('censor_proper', '$censored_proper')");
----}
----#
-
-/******************************************************************************/
---- Converting moderators...
-/******************************************************************************/
-
-TRUNCATE {$to_prefix}moderators;
-
----# Moving censored words...
----{
-$result = convert_query("
-	SELECT member_id AS id_member, forum_id AS id_board
-	FROM {$from_prefix}moderators
-	WHERE member_id != -1;");
-$ignore = true;
-
-$mods = array();
-while ($row = convert_fetch_assoc($result))
-{
-	if (empty($row['id_member']))
-		continue;
-	if (strpos($row['id_board'], ',') !== false)
-	{
-		$tmpb = explode(',', $row['id_board']);
-		foreach ($tmpb as $b)
-		{
-			$b = trim($b);
-			if (!empty($b))
-				$mods[] = array(
-					'id_member' => $row['id_member'],
-					'id_board' => $b
-				);
-		}
-	}
-}
-convert_free_result($result);
-
-if (!empty($mods))
-	foreach ($mods as $row)
-		convert_query("
-		INSERT INTO {$to_prefix}moderators
-			(id_board, id_member)
-		VALUES (" . $row['id_board'] . ", " . $row['id_member'] . ")");
----}
----#
 
 /******************************************************************************/
 --- Converting smileys (step 1)...
@@ -531,7 +468,7 @@ if (!empty($rows))
 SELECT
 	typed as code, image as filename, image as description, 0 as smiley_row,
 	100 as smiley_order, 1 as hidden
-FROM {$from_prefix}emoticons;
+FROM {$from_prefix}core_emoticons;
 ---*
 
 /******************************************************************************/
@@ -601,15 +538,17 @@ if (copy($oldAttachmentDir . '/' . $row['attach_location'], $attachmentUploadDir
 	$id_attach++;
 }
 ---}
-SELECT attach_rel_id AS id_msg, attach_hits, attach_file AS filename, attach_filesize AS filesize, attach_location
-FROM {$from_prefix}attachments;
+SELECT m.id2 as id_msg, a.attach_hits, a.attach_file AS filename, a.attach_filesize AS filesize, a.attach_location
+FROM {$from_prefix}core_attachments as a
+LEFT JOIN {$from_prefix}core_attachments_map m ON (m.attachment_id = a.attach_id)
+WHERE m.location_key = 'forums_Forums';
 ---*
 
 /******************************************************************************/
 --- Converting user avatars...
 /******************************************************************************/
 
----* {$to_prefix}attachments
+---* {$to_prefix}core_attachments
 ---{
 $no_add = true;
 $keys = array('id_attach', 'size', 'filename', 'id_member', 'width', 'height', 'attachment_type');
@@ -683,7 +622,9 @@ if (strlen($smf_avatar_filename) <= 255 && copy($ipb_avatar, $avatar_dir . '/' .
 	++$id_attach;
 
 	// Get width, height, filename and ID_MEMBER
-	list ($width, $height) = explode('x', $row['dimension']);
+	//list ($width, $height) = explode('x', $row['dimension']);
+	$height = $row['height'];
+	$width = $row['width']; 
 	$filesize = filesize($ipb_avatar);
 	
         $rows[] = array(
@@ -697,9 +638,9 @@ if (strlen($smf_avatar_filename) <= 255 && copy($ipb_avatar, $avatar_dir . '/' .
 	);
 }
 ---}
-SELECT pp_member_id AS id_member, avatar_location AS filename, avatar_size AS dimension
-FROM {$from_prefix}profile_portal
-WHERE avatar_type = 'upload';
+SELECT member_id AS id_member, pp_main_photo AS filename, pp_main_width as width, pp_main_height as height 
+FROM {$from_prefix}core_members
+
 ---*
 
 /******************************************************************************/
@@ -762,7 +703,7 @@ convert_free_result($result);
 
 $result = convert_query("
 	SELECT cs_value
-	FROM {$from_prefix}cache_store
+	FROM {$from_prefix}core_cache_store
 		WHERE cs_key = 'stats'");
 list ($inv_stats) = convert_fetch_row($result);
 $inv_stats = unserialize($inv_stats);
